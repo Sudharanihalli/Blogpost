518c4d076c0229fd03b33e63f947ff63fd7fbfdf
Sudharanihalli <113384546+Sudharanihalli@users.noreply.github.com>
2023-10-17 17:56:34 +0530

    update4

--- app/controllers/concerns/blog_posts_controller.rb
+++ app/controllers/concerns/blog_posts_controller.rb
@@ -8,3 +8,23 @@ class BlogpostController < ApplicationController
     rescue ActiveRecord::RecordNotFound
         redirect_to root_path
 end
+
+def new
+    @blog_post = Blogpost.new
+end
+
+def create
+    @blog_post = Blogpost.new(blog_post_param)
+    @blog_post.save
+    redirect_to @blog_post
+else
+    render :new, status: :unprocessable_entity
+end
+end
+
+private
+
+def blog_post_params
+    params.require(:blog_post).permit(:title, :body)
+end
+end  

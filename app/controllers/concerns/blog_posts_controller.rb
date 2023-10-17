6752a7490561038a0f2d55fd77f63640d8d612c2
Sudharanihalli <113384546+Sudharanihalli@users.noreply.github.com>
2023-10-17 14:41:43 +0530

    update3

--- app/controllers/concerns/blog_posts_controller.rb
+++ app/controllers/concerns/blog_posts_controller.rb
@@ -2,4 +2,9 @@ class BlogpostController < ApplicationController
     def index
         @blog_posts = Blogpost.all
     end
+
+    def show
+        @blog_post = Blogpost.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+        redirect_to root_path
 end
